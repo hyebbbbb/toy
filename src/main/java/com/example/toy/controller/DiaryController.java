41d30879ea43284cbb3a5ac8bd9ced87a68fc9bd
hyeb <designhyeb@gmail.com>
2023-11-09 22:02:30 +0900

    일기장 추가

--- src/main/java/com/example/toy/controller/DiaryController.java
+++ src/main/java/com/example/toy/controller/DiaryController.java
@@ -2,13 +2,16 @@ package com.example.toy.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.toy.model.Diary;
 import com.example.toy.repository.DiaryRepository;
@@ -17,12 +20,15 @@ import com.example.toy.repository.DiaryRepository;
 public class DiaryController {
     @Autowired
     DiaryRepository diaryRepository;
-    
-    @GetMapping("/diaryview")
-    public String diaryview() {
+
+    @GetMapping("/diaryview/{id}")
+    public String diaryview(Model model, @PathVariable("id") long id) {
+        Optional<Diary> view = diaryRepository.findById(id);
+        Diary diary = view.get();
+        model.addAttribute("diary", diary);
         return "/diary/diaryview";
     }
-    
+
     // 조회하는 기능?
     @GetMapping("/diarylist")
     public String diarylist(Model model) {
@@ -30,6 +36,12 @@ public class DiaryController {
         model.addAttribute("diaryList", diaryList);
         return "/diary/diarylist";
     }
+
+    // @PostMapping("/diarylist")
+    // public String diarylistPost(@ModelAttribute Diary diary) {
+    //     diaryRepository.save(diary);
+    //     return "redirect:/diary/diarylist"; // POST 요청 처리 후 다른 페이지로 리다이렉트
+    // }
 
     @GetMapping("/diary")
     public String diary() {
@@ -40,6 +52,6 @@ public class DiaryController {
     public String diaryPost(@ModelAttribute Diary diary) {
         diary.setCreDate(new Date());
         diaryRepository.save(diary);
-        return "redirect:/";
+        return "redirect:/diarylist";
     }
 }
--- src/main/java/com/example/toy/repository/DiaryRepository.java
+++ src/main/java/com/example/toy/repository/DiaryRepository.java
@@ -3,6 +3,9 @@ package com.example.toy.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.toy.model.Diary;
+import java.util.Optional;
+
 
 public interface DiaryRepository extends JpaRepository<Diary, Long>{
+    Optional<Diary> findById(long id);
 }
